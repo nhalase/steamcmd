54286a67b9586b09f4ef055d47b1779fc2f10ad6
Nick Halase <4644530+nhalase@users.noreply.github.com>
2023-04-27 02:54:53 +0000

    format files and add shared settings for consistency

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,19 +4,19 @@ set -e
 
 # Change the UID if needed
 if [ ! "$(id -u steam)" -eq "$UID" ]; then
-    echo "Changing steam uid to $UID."
-    usermod -o -u "$UID" steam
+  echo "Changing steam uid to $UID."
+  usermod -o -u "$UID" steam
 fi
 
 # Change GID if needed
 if [ ! "$(id -g steam)" -eq "$GID" ]; then
-    echo "Changing steam gid to $GID."
-    groupmod -o -g "$GID" steam
+  echo "Changing steam gid to $GID."
+  groupmod -o -g "$GID" steam
 fi
 
 if [ "$1" = '/home/steam/steamcmd/steamcmd.sh' ] || [ "$1" = 'steamcmd' ] || [ "$1" = 'steamcmd.sh' ]; then
-    shift
-    su-exec "$UID":"$GID" /home/steam/steamcmd/steamcmd.sh "$@"
+  shift
+  su-exec "$UID":"$GID" /home/steam/steamcmd/steamcmd.sh "$@"
 else
-    su-exec "$UID":"$GID" "$@"
+  su-exec "$UID":"$GID" "$@"
 fi
